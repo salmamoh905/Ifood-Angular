b9a8ef6c8ba3ef1eff084f3798a426db2218336f
salma <salamamoh246@gmail.com>
2022-06-21 09:57:56 +0300

    added the food detail class and services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {FoodService} from '../services/food/food.service'
+import {Foods} from '../shared/models/food'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  foods:string[] =[];
+  foods:Foods[] =[];
 
   constructor(private fs:FoodService) { }
 
--- src/app/services/food/food.service.ts
+++ src/app/services/food/food.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Foods } from 'src/app/shared/models/food';
 
 @Injectable({
   providedIn: 'root'
@@ -6,11 +7,63 @@ import { Injectable } from '@angular/core';
 export class FoodService {
 
   constructor() { }
-  getAll():string[]{
-    return [
-      '/assets/burger.jpg',
-      '/assets/chicken.jpg',
-      '/assets/meatballs.jpg',
+  // getAll():string[]{
+  //   return [
+  //     '/assets/cakes.jpeg',
+  //     '/assets/burger.jpg',
+  //     '/assets/roast.jpeg',
+  //     '/assets/vegpex.jpeg',
+
+  //   ]
+  // }
+  getAll():Foods[]{
+    return[
+      {
+        id:1,
+        name: 'food',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['italy'],
+        stars:4.5,
+        imageUrl: '/assets/cakes.jpeg',
+        tags:['FastFood', 'Pizza','Lunch'],
+      },
+      {
+        id:2,
+        name: 'food',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['Somalian'],
+        stars:4.5,
+        imageUrl: '/assets/burger.jpg',
+        tags:['FastFood', 'Pizza','Lunch'],
+
+      },
+      {
+        id:3,
+        name: 'food',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['India'],
+        stars:4.5,
+        imageUrl: '/assets/roast.jpeg',
+        tags:['FastFood', 'Pizza','Lunch'],
+
+      },
+      {
+        id:4,
+        name: 'food',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['Spain'],
+        stars:4.5,
+        imageUrl: '/assets/vegpex.jpeg',
+        tags:['FastFood', 'Pizza','Lunch'],
+      }
     ]
   }
 }
